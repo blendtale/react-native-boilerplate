7676b8562b2539b0f81c79e9ba21b2d456d6951f
irohitb <irohitbhatia@gmail.com>
2021-04-05 16:32:32 +0530

    feat: updated dependencies

--- ios/blendtaleTests/blendtaleTests.m
+++ ios/blendtaleTests/blendtaleTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface virtualisedListTests : XCTestCase
+@interface blendtaleTests : XCTestCase
 
 @end
 
-@implementation virtualisedListTests
+@implementation blendtaleTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
